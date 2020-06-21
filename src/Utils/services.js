8f3597a9d9b4b5dcef7ef11bdaa0c0d5751cf547
unknown <lh.pedrosot@hotmail.com>
2020-06-21 10:22:16 -0300

    no /

--- src/Utils/services.js
+++ src/Utils/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://gadablog-rest-api.herokuapp.com/'
+  baseURL: 'http://gadablog-rest-api.herokuapp.com'
 })
 
 export default api
